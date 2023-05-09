19d7822d6574da78e8512899fedc8dc8123e27a8
Aleksandra Gontar <a.k.gontar02@mail.ru>
2023-05-09 12:48:05 +0800

    input checking

--- min knf code/minknf/Program.cs
+++ min knf code/minknf/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 
 namespace minknf
@@ -14,11 +15,43 @@ namespace minknf
             sknf = "(x1vx2v-x3v-x4)&(x1v-x2vx3vx4)&(x1v-x2vx3v-x4)&(x1v-x2v-x3v-x4)&(-x1vx2vx3vx4)&(-x1vx2vx3v-x4)&(-x1vx2v-x3vx4)&(-x1vx2v-x3v-x4)&(-x1v-x2vx3vx4)&(-x1v-x2vx3v-x4)";
             //sknf = "(x1vx2v-x3)&(x1v-x2vx3)&(-x1v-x2v-x3)&(x1v-x2v-x3)";
             //sknf = "(x1vx2v-x3v-x4vx5vx6)&(x1v-x2vx3vx4vx5v-x6)&(x1v-x2vx3v-x4vx5vx6)&(x1v-x2v-x3v-x4vx5vx6)&(-x1vx2vx3vx4vx5vx6)&(-x1vx2vx3v-x4vx5vx6)&(-x1vx2v-x3vx4v-x5vx6)&(-x1vx2v-x3v-x4vx5vx6)&(-x1v-x2vx3vx4v-x5vx6)&(-x1v-x2vx3v-x4vx5vx6)";
-
-            KNF knf = new KNF(sknf);
-            knf.MinimizeKnf();
-            Console.WriteLine(knf.ToString());
+            Console.WriteLine(StringChecking(sknf));
+            // KNF knf = new KNF(sknf);
+            // knf.MinimizeKnf();
+            // Console.WriteLine(knf.ToString());
             //Console.WriteLine(10_000 + 1);
+        }
+
+        private static Boolean StringChecking(string str)
+        {
+            str = str.Replace("(", String.Empty);
+            str = str.Replace(")", String.Empty);
+            string[] strings = str.Split("&");
+            string heh = strings[strings.Length - 1];
+            int n = heh[heh.Length - 1]- '0';
+            foreach (var item in strings){
+                if (!Regex.IsMatch(item, "[\\-xv0-9-]+") || !char.IsNumber(item[item.Length - 1]) || item[item.Length - 1] - '0' != n){
+                    return false;
+                }
+                n = item[item.Length - 1] - '0';
+                
+                string[] bul = item.Split("v");
+                if (bul.Length != n){
+                    return false;
+                }
+                
+                for (int i = 0; i < n; i++){
+                    string temp_string = bul[i];
+                    if (!Regex.IsMatch(temp_string, "[x0-9]+|[\\-x0-9]+"))
+                    {
+                        return false;
+                    }
+                    if ((temp_string[temp_string.Length - 1] - '0') != (i+1)){
+                        return false;
+                    }
+                }
+            }
+            return true;
         }
     }
 }
