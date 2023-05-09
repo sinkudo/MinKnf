4a8fb28f59809b7ef5d2794cf99a21f08620f59b
vrkh <99482019+vrkh@users.noreply.github.com>
2023-05-10 00:07:31 +0800

    Update Program.cs, add example of using GA

--- min knf code/minknf/Program.cs
+++ min knf code/minknf/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -15,12 +15,26 @@ namespace minknf
             sknf = "(x1vx2v-x3v-x4)&(x1v-x2vx3vx4)&(x1v-x2vx3v-x4)&(x1v-x2v-x3v-x4)&(-x1vx2vx3vx4)&(-x1vx2vx3v-x4)&(-x1vx2v-x3vx4)&(-x1vx2v-x3v-x4)&(-x1v-x2vx3vx4)&(-x1v-x2vx3v-x4)";
             //sknf = "(x1vx2v-x3)&(x1v-x2vx3)&(-x1v-x2v-x3)&(x1v-x2v-x3)";
             //sknf = "(x1vx2v-x3v-x4vx5vx6)&(x1v-x2vx3vx4vx5v-x6)&(x1v-x2vx3v-x4vx5vx6)&(x1v-x2v-x3v-x4vx5vx6)&(-x1vx2vx3vx4vx5vx6)&(-x1vx2vx3v-x4vx5vx6)&(-x1vx2v-x3vx4v-x5vx6)&(-x1vx2v-x3v-x4vx5vx6)&(-x1v-x2vx3vx4v-x5vx6)&(-x1v-x2vx3v-x4vx5vx6)";
-            Console.WriteLine(StringChecking(sknf));
-            // KNF knf = new KNF(sknf);
-            // knf.MinimizeKnf();
-            // Console.WriteLine(knf.ToString());
+            //Console.WriteLine(StringChecking(sknf));
+            //KNF knf = new KNF(sknf);
+            //knf.MinimizeKnf();
+            //Console.WriteLine(knf.ToString());
             //Console.WriteLine(10_000 + 1);
-        }
+            var data = new Dictionary<string, string>()
+            {
+                {"epochs", "10000"},
+                {"mutationChance", "1"},
+                {"crossoverChance", "1"},
+                {"populationSize", "100"},
+            };
+            int[,] a = new int[2, 2];
+            GA ga = new GA(a, data);
+            Console.WriteLine(ga.epochs);
+            Console.WriteLine(ga.mutationChance);
+            Console.WriteLine(ga.crossoverChance);
+            Console.WriteLine(ga.populationSize);
+
+    }
 
         private static Boolean StringChecking(string str)
         {
