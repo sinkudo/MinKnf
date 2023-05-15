f230944025521ee421e2076a2fd8f36f693f23fc
vrkh <99482019+vrkh@users.noreply.github.com>
2023-05-15 20:07:34 +0800

    Update Program.cs

--- min knf code/minknf/Program.cs
+++ min knf code/minknf/Program.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text.RegularExpressions;
 
 
@@ -10,31 +12,58 @@ namespace minknf
     {
         static void Main(string[] args)
         {
-            String sknf = "(x1vx2vx3)&(x1vx2v-x3)&(-x1vx2vx3)&(x1v-x2vx3)";
+            //var data = new Dictionary<string, string>()
+            //{
+            //    {"epochs", "10000"},
+            //    {"mutationChance", "1"},
+            //    {"crossoverChance", "1"},
+            //    {"populationSize", "100"},
+            //};
+            //int[,] a = new int[2, 2];
+            //GA ga = new GA(a, data);
+            //Console.WriteLine(ga.epochs);
+            //Console.WriteLine(ga.mutationChance);
+            //Console.WriteLine(ga.crossoverChance);
+            //Console.WriteLine(ga.populationSize);
+
+            // sknfPath paramsPath
+
+            string currentDirectory = Environment.CurrentDirectory;
+            string paramsFilePath = Path.Combine(currentDirectory, "params.txt");
+            Console.WriteLine(paramsFilePath);
+            Dictionary<string, string> data = FileParser.Parse(paramsFilePath);
+            if (data == null)
+            {
+                System.Environment.Exit(1);
+            }
+
+            string sknfFilePath = Path.Combine(currentDirectory, "sknf.txt");
+            String sknf = File.ReadAllText(sknfFilePath);
+
+            String outFilePath = Path.Combine(currentDirectory, "out.txt");
+            //String sknf = "(x1vx2vx3)&(x1vx2v-x3)&(-x1vx2vx3)&(x1v-x2vx3)";
             //sknf = "(x1vx2vx3)&(x1vx2v-x3)&(-x1v-x2v-x3)";
-            sknf = "(x1vx2v-x3v-x4)&(x1v-x2vx3vx4)&(x1v-x2vx3v-x4)&(x1v-x2v-x3v-x4)&(-x1vx2vx3vx4)&(-x1vx2vx3v-x4)&(-x1vx2v-x3vx4)&(-x1vx2v-x3v-x4)&(-x1v-x2vx3vx4)&(-x1v-x2vx3v-x4)";
+            //sknf = "(x1vx2v-x3v-x4)&(x1v-x2vx3vx4)&(x1v-x2vx3v-x4)&(x1v-x2v-x3v-x4)&(-x1vx2vx3vx4)&(-x1vx2vx3v-x4)&(-x1vx2v-x3vx4)&(-x1vx2v-x3v-x4)&(-x1v-x2vx3vx4)&(-x1v-x2vx3v-x4)";
             //sknf = "(x1vx2v-x3)&(x1v-x2vx3)&(-x1v-x2v-x3)&(x1v-x2v-x3)";
             //sknf = "(x1vx2v-x3v-x4vx5vx6)&(x1v-x2vx3vx4vx5v-x6)&(x1v-x2vx3v-x4vx5vx6)&(x1v-x2v-x3v-x4vx5vx6)&(-x1vx2vx3vx4vx5vx6)&(-x1vx2vx3v-x4vx5vx6)&(-x1vx2v-x3vx4v-x5vx6)&(-x1vx2v-x3v-x4vx5vx6)&(-x1v-x2vx3vx4v-x5vx6)&(-x1v-x2vx3v-x4vx5vx6)";
             //Console.WriteLine(StringChecking(sknf));
-            //KNF knf = new KNF(sknf);
-            //knf.MinimizeKnf();
-            //Console.WriteLine(knf.ToString());
-            //Console.WriteLine(10_000 + 1);
-            var data = new Dictionary<string, string>()
+            if (StringChecking(sknf))
             {
-                {"epochs", "10000"},
-                {"mutationChance", "1"},
-                {"crossoverChance", "1"},
-                {"populationSize", "100"},
-            };
-            int[,] a = new int[2, 2];
-            GA ga = new GA(a, data);
-            Console.WriteLine(ga.epochs);
-            Console.WriteLine(ga.mutationChance);
-            Console.WriteLine(ga.crossoverChance);
-            Console.WriteLine(ga.populationSize);
-
-    }
+                KNF knf = new KNF(sknf, data);
+                knf.MinimizeKnf();
+                try
+                {
+                    File.WriteAllText(outFilePath, knf.ToString());
+                    Console.WriteLine("String successfully written to the file.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred while writing to the file: " + ex.Message);
+                }
+                //Console.WriteLine(knf.ToString());
+            }
+            //Console.WriteLine(10_000 + 1);
+        }
 
         private static Boolean StringChecking(string str)
         {
