b39f3309f1219dcd8b189bcf589c1e06b018a6de
vrkh <99482019+vrkh@users.noreply.github.com>
2023-05-20 17:59:08 +0800

    add exceptions (try catch)

--- min knf code/minknf/FileParser.cs
+++ min knf code/minknf/FileParser.cs
@@ -10,18 +10,30 @@ namespace minknf
         public static Dictionary<string, string> Parse(string filePath)
         {
             Dictionary<string, string> fileData = new Dictionary<string, string>();
-            StreamReader reader = new StreamReader(filePath);
-            string fileContents = reader.ReadToEnd();
-            reader.Close();
-            string[] lines = fileContents.Split('\n');
-            foreach (string line in lines)
+            try
             {
-                string[] parts = line.Trim().Split(':');
-                if (parts.Length == 2)
+                using (StreamReader reader = new StreamReader(filePath))
                 {
-                    fileData[parts[0].Trim()] = parts[1].Trim();
+                    string fileContents = reader.ReadToEnd();
+                    if (fileContents == String.Empty) {
+                        return null;
+                    }
+                    string[] lines = fileContents.Split('\n');
+                    foreach (string line in lines)
+                    {
+                        string[] parts = line.Trim().Split(':');
+                        if (parts.Length == 2)
+                        {
+                            fileData[parts[0].Trim()] = parts[1].Trim();
+                        }
+                    }
                 }
             }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
             return fileData;
         }
     }
