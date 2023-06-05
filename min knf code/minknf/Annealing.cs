1afc1c302cb2add75f35fa277df327cf000e12d9
sinkudo <mishazagorodnikov@mail.ru>
2023-06-05 20:38:45 +0800

    другая Fкач

--- min knf code/minknf/Annealing.cs
+++ min knf code/minknf/Annealing.cs
@@ -12,60 +12,71 @@ namespace minknf
         List<DisjunctiveMonomial> monomials;
         double T = 100;
         double currentT = 100;
+        double Fopt;
+        double Fbeg;
         List<int> canditateInds = new List<int>();
+        List<int> brusok = new List<int>();
+        int brusokSize;
         public Annealing(int[,] matrix, List<int> absoluteIndexes, List<DisjunctiveMonomial> monomials): base(matrix, absoluteIndexes)
         {
+            
             this.monomials = monomials;
             for(int i = 0; i < pool.Length; i++)
             {
                 if (pool[i].Count > 1)
                     canditateInds.Add(i);
             }
+            //foreach(var i in pool)
+            //    foreach(var j in i)
+            //        Console.WriteLine(j);
+            brusok = CreateBrucock(pool);
+            PrintBrucock(" ", brusok);
+            brusokSize = GetBrucockSize(brusok);
         }
         public List<int> Anneal()
         {
             // Brucock (брусок) - это list рандомных значений из pool
             // atom - эл бруска 
-            List<int> currentBrucock = CreateBrucock(pool);
-            PrintBrucock("Брусок: ", currentBrucock);
-            int currentSize = GetBrucockSize(currentBrucock);
+            PrintBrucock("Брусок: ", brusok);
+            int currentSize = GetBrucockSize(brusok);
             Console.WriteLine("Size: " + currentSize);
             Console.WriteLine("---");
 
             while (currentT > 0.1)
             {
-                List<int> newBrucock = ChangeBrucock(currentBrucock);
-                PrintBrucock("кандидат брусок: ", newBrucock);
+                List<int> newBrucock = ChangeBrucock(brusok);
+                //PrintBrucock("кандидат брусок: ", newBrucock);
                 int newBrucockSize = GetBrucockSize(newBrucock);
                 Console.WriteLine("New Brusok Size: " + newBrucockSize);
                 // функция энергии
-                int sizeDiffence = newBrucockSize - GetBrucockSize(currentBrucock);
-                Console.WriteLine(sizeDiffence);
-                if (sizeDiffence <= 0)
+                //int sizeDiffence = newBrucockSize - GetBrucockSize(currentBrucock);
+                double energyDif = BrucockEnergy(newBrucock) - BrucockEnergy(brusok);
+
+                if (energyDif <= 0)
                 {
-                    currentBrucock = newBrucock;
-                    PrintBrucock("size<=0, current brusok = ", currentBrucock);
+                    brusok = newBrucock;
+                    PrintBrucock("size<=0, current brusok = ", brusok);
                 }
                 else
                 {
                     Console.WriteLine("size>0");
-                    double polikarpia = Math.Exp(-sizeDiffence / currentT);
+                    double polikarpia = Math.Exp(-energyDif / currentT);
                     double p = random.NextDouble();
                     Console.WriteLine(polikarpia + ", " + p);
                     
                     if (polikarpia > p)
                     {
-                        currentBrucock = newBrucock;
+                        brusok = newBrucock;
                     }
                 }
-                PrintBrucock("Itog brusok: ", currentBrucock);
-                Console.WriteLine("Itog Size: " + GetBrucockSize(currentBrucock));
+                PrintBrucock("Itog brusok: ", brusok);
+                Console.WriteLine("Itog Size: " + GetBrucockSize(brusok));
                 Console.WriteLine("---");
                 currentT -= 0.1;
                 //currentT *= 0.5;
                 // 0.5 * (T_i / T0)
             }
-            return ToAbsolute(currentBrucock);
+            return ToAbsolute(brusok);
         }
 
         private List<int> ChangeBrucock(List<int> brucock)
@@ -75,7 +86,7 @@ namespace minknf
             //int count = (int)(0.5 * (currentT / T) * brucock.Count);
             int count = (int)Math.Ceiling((0.5 * (currentT / T) * brucock.Count));
 
-            Console.WriteLine("count " +count);
+            //Console.WriteLine("count " +count);
 
             //List<int> tmp = Enumerable.Range(0, brucock.Count).OrderBy(x => random.Next()).Take(count).ToList();
             List<int> tmp = new List<int>();
@@ -83,8 +94,8 @@ namespace minknf
                 tmp = canditateInds;
             else
                 tmp = canditateInds.OrderBy(x => random.Next()).Take(count).ToList();
-            PrintBrucock("brucock ", newBrucock);
-            PrintBrucock("tmp is ", tmp);
+            //PrintBrucock("brucock ", newBrucock);
+            //PrintBrucock("tmp is ", tmp);
             foreach (var i in tmp)
             {
                 List<int> candidates = new List<int>(pool[i]);
@@ -127,6 +138,12 @@ namespace minknf
                 size += monomials[atom].GetSize();
             }
             return size;
+        }
+        private double BrucockEnergy(List<int> brusok)
+        {
+            //Console.WriteLine((double)GetBrucockSize(brusok) / (double)brusokSize);
+            //Console.WriteLine(GetBrucockSize(brusok) + " " + brusokSize);
+            return (double)GetBrucockSize(brusok) / (double)brusokSize;
         }
         private List<int> ToAbsolute(List<int> brusok)
         {
