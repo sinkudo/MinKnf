9929282ea456e4ab56fa40be00448ab27e6441f3
vrkh <99482019+vrkh@users.noreply.github.com>
2023-06-05 23:19:52 +0800

    add FileWorker

---
+++ min knf code/minknf/FileWorker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace minknf
+{
+    class FileWorker
+    {
+        public static void SaveData(List<int> data)
+        {
+            // Specify the file path
+            string currentDirectory = Environment.CurrentDirectory;
+            string dataFilePath = Path.Combine(currentDirectory, "data.txt");
+
+            // Convert the List<int> to a string representation
+            string dataString = string.Join(Environment.NewLine, data);
+
+            // Write the numbers to the file
+            File.WriteAllText(dataFilePath, dataString);
+        }
+    }
+}
