0fb9a32761928a1d1d465b0f501dc20e320356eb
vrkh <99482019+vrkh@users.noreply.github.com>
2023-06-08 20:54:19 +0800

    add ReadParameterFile func

--- min knf code/minknf/FileWorker.cs
+++ min knf code/minknf/FileWorker.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
@@ -18,6 +18,34 @@ namespace minknf
 
             // Write the numbers to the file
             File.WriteAllText(dataFilePath, dataString);
+        }
+
+        public static Dictionary<string, string> ReadParameterFile(string filePath)
+        {
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split(':');
+
+                    if (parts.Length == 2)
+                    {
+                        string key = parts[0].Trim();
+                        string value = parts[1].Trim();
+
+                        parameters[key] = value;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error reading parameter file: " + ex.Message);
+            }
+
+            return parameters;
         }
     }
 }
